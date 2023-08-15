58766eb049bd1517531d90988cef26c8aa0f65ea
Christopher Hardy <chris.hardy1989@hotmail.com>
2023-08-15 16:58:39 -0400

    Add Subheader.js

---
+++ src/components/fields/Subheader.js
@@ -0,0 +1,24 @@
+/**
+ * @file Subheader.js
+ * @author 0xChristopher
+ * @brief This file is responsible for the Subheader component of the CRM website.
+ */
+
+/**
+ * @brief The Subheader() function builds a subheader field component.
+ * @return Returns the subheader field component to be added to the page
+ */
+function Subheader(props)
+{
+    const {
+        label
+    } = props
+
+    return (
+        <div className="form--subheader">
+            {label}
+        </div>
+    )
+}
+
+export default Subheader
